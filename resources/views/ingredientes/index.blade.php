77135ed0a848acfda8900c219b33b9dbb608693c
Diego Tsuyoshi <dttsuyoshi@gmail.com>
2017-06-04 16:23:46 -0400

    Adicionado cabeçalho de tabela no ingredientes.index

--- resources/views/ingredientes/index.blade.php
+++ resources/views/ingredientes/index.blade.php
@@ -4,27 +4,36 @@
     <h1>Lista de Ingredientes</h1>
     <a class="btn btn-primary" href="{{ route('ingredientes.create') }}">Cadastrar Ingrediente</a><hr>
     <table class="table">
-        @forelse($ingredientes as $ingrediente)
+        <thead>
             <tr>
-                <td>{{ $ingrediente->nome }}</td>
-                <td>{{ $ingrediente->preco }}</td>
-                @if($ingrediente->disponivel)
-                    <td>Disponível</td>
-                @else
-                    <td>Indisponível</td>
-                @endif
-
-                <td>
-                    <a class="btn btn-primary" href="{{ route('ingredientes.edit', $ingrediente->id) }}" >Editar</a>
-                    {!! Form::open(['method' => 'DELETE','route' => ['ingredientes.destroy', $ingrediente->id],'style'=>'display:inline']) !!}
-                    {!! Form::submit('Deletar', ['class' => 'btn btn-danger']) !!}
-                    {!! Form::close() !!}
-                </td>
+                <th>Nome</th>
+                <th>Preço</th>
+                <th>Disponibilidade</th>
+                <th>Ações</th>
             </tr>
+        </thead>
+        <tbody>
+            @forelse($ingredientes as $ingrediente)
+                <tr>
+                    <td>{{ $ingrediente->nome }}</td>
+                    <td>{{ $ingrediente->preco }}</td>
+                    @if($ingrediente->disponivel)
+                        <td>Disponível</td>
+                    @else
+                        <td>Indisponível</td>
+                    @endif
 
-        @empty
-            <p>Nenhum ingrediente cadastrado!</p>
-            
-        @endforelse
+                    <td>
+                        <a class="btn btn-primary" href="{{ route('ingredientes.edit', $ingrediente->id) }}" >Editar</a>
+                        {!! Form::open(['method' => 'DELETE','route' => ['ingredientes.destroy', $ingrediente->id],'style'=>'display:inline']) !!}
+                        {!! Form::submit('Deletar', ['class' => 'btn btn-danger']) !!}
+                        {!! Form::close() !!}
+                    </td>
+                </tr>
+            @empty
+                <p>Nenhum ingrediente cadastrado!</p>
+
+            @endforelse
+        </tbody>
     </table>
 @stop
