034778cba3d6d2ef39e632bf46633537e384f988
Diego Tsuyoshi <dttsuyoshi@gmail.com>
2017-06-13 10:10:05 -0400

    view pizza/index criada

--- resources/views/pizzas/index.blade.php
+++ resources/views/pizzas/index.blade.php
@@ -1,2 +1,33 @@
 @extends('layouts.master')
 
+@section('content')
+    <h1>Cardápio</h1>
+    <a class="btn btn-primary" href="{{ route('pizzas.create') }}">Cadastrar Pizza</a>
+    <hr>
+    <table class="table">
+        <thead>
+        <tr>
+            <th>Nome</th>
+            <th>Preço</th>
+            <th>Ações</th>
+        </tr>
+        </thead>
+        <tbody>
+        @forelse($pizzas as $pizza)
+            <tr>
+                <td>{{ $pizza->nome }}</td>
+                <td>{{ $pizza->preco }}</td>
+                <td>
+                    <a class="btn btn-primary" href="{{ route('pizzas.edit', $pizza->id) }}">Editar</a>
+                    {!! Form::open(['method' => 'DELETE','route' => ['pizzas.destroy', $pizza->id],'style'=>'display:inline']) !!}
+                    {!! Form::submit('Deletar', ['class' => 'btn btn-danger']) !!}
+                    {!! Form::close() !!}
+                </td>
+            </tr>
+        @empty
+            <p>Nenhuma pizza cadastrada!</p>
+
+        @endforelse
+        </tbody>
+    </table>
+@stop
