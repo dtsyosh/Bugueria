397fb30362c9ab9ec0e2841615d3f4356b4ba405
Diego Tsuyoshi <dttsuyoshi@gmail.com>
2017-06-13 14:43:16 -0400

    Alterações de visual

--- resources/views/ingredientes/create.blade.php
+++ resources/views/ingredientes/create.blade.php
@@ -1,35 +1,45 @@
 @extends('layouts.master')
 
-@section('title', 'Criar novo produto')
 
 
 @section('content')
-    <h1>Criar novo produto</h1>
+    <h1>Criar novo ingrediente</h1>
 
     {!! Form::open(['route' => 'ingredientes.store']) !!}
+
     <div class="form-group">
         {!! Form::label('Nome') !!}
         {!! Form::text('nome', null, ['class' => 'form-control', 'required']) !!}
     </div>
 
     <div class="form-group">
-        {!! Form::label('Preço de uma porção') !!}
+        {!! Form::label('Preço (*)') !!}
         {!! Form::text('preco_porcao', null, ['class' => 'form-control', 'required']) !!}
     </div>
 
     <div class="form-group">
-        {!! Form::label('Quantidade de uma porção') !!}
+        {!! Form::label('Quantidade (*)') !!}
         {!! Form::text('qtde_porcao', null, ['class' => 'form-control', 'required']) !!}
-        {!! Form::select('unidade', ['ml' => 'ml', 'mg' => 'mg']) !!}
+        {!! Form::select('unidade', ['ml' => 'ml', 'grama' => 'g']) !!}
     </div>
 
     <div class="form-group">
         {!! Form::label('Quantidade total:') !!}
         {!! Form::text('qtde_total', null, ['class' => 'form-control']) !!}
     </div>
-    {!! Form::submit('Cadastrar', ['class' => 'btn btn-primary']) !!}
+    <div class="row">
+        <div class="col-md-4">
+        {!! Form::submit('Cadastrar', ['class' => 'btn btn-primary']) !!}
+        </div>
+
+        <div class="col-md-offset-11">
+            <small class="text-muted"><i><b>* 1 porção</b></i></small>
+        </div>
+    </div>
+
 
 
     {{--Mensagens de erro--}}
     @include('layouts/errors')
     {!! Form::close() !!}
+@endsection
